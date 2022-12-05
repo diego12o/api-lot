a858f6a16c8292ad3b8c4422b89ba671fd300ce7
Diego Alonso Candia <diego.alonso1@mail.udp.cl>
2022-12-04 21:09:02 -0300

    Falta probar sensor_data

--- db/query_sensor_data.py
+++ db/query_sensor_data.py
@@ -11,17 +11,20 @@ def query_sensor_data(from_, to, sensor_id):
 
     # QUERY
 
-    data = {}
+    data = []
+
+    data_json = []
 
     for id in sensor_id:
         query = "SELECT SENSOR_DATA.TIME, SENSOR_DATA.HUMIDITY, SENSOR_DATA.TEMPERATURE, SENSOR_DATA.DISTANCE, SENSOR_DATA.PRESSURE, SENSOR_DATA.LIGHT_LEVEL, COMPANY.ID, LOCATION.ID, SENSOR.ID FROM COMPANY, LOCATION, SENSOR, SENSOR_DATA WHERE SENSOR.SENSOR_API_KEY = SENSOR_DATA.SENSOR_API_KEY AND LOCATION.COMPANY_ID = COMPANY.ID AND SENSOR.LOCATION_ID = LOCATION.ID AND SENSOR.ID = " + str(id) + " AND SENSOR_DATA.TIME <= " + str(to) + " AND SENSOR_DATA.TIME >= " + str(from_)
 
         result = cursor.execute(query)
-
         for row in result:
-            print(row)
-
-        data = {
-            id : [result.fetchall()]
+            data.append(row)
+        
+        json = {
+            id: data
         }
-    return data
+
+        data_json.append(json)
+    return data_json
